bf366871a2e5a4c576f893c25b35f811ca1d8218
oldeus <kenimottin@gmail.com>
2025-11-26 15:51:44 +0100

    fin page accueil

--- ReactDandata/src/App.jsx
+++ ReactDandata/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
+import {Footer} from "./components/Footer";
+
 import {Accueil} from "./pages/Accueil";
 import {Categories} from "./pages/Categories";
 import {LogIn} from "./pages/LogIn";
 import {SignIn} from "./pages/SignIn";
 import {Dashboard} from "./pages/Dashboard";
+
 import "./App.css"
 
 function App() {
@@ -19,6 +22,8 @@ function App() {
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
+      <Footer />
+
     </div>
   );
 }
--- ReactDandata/src/pages/Accueil.jsx
+++ ReactDandata/src/pages/Accueil.jsx
@@ -1,9 +1,10 @@
 import LightRays from "../components/LightRays";
 import ModelViewer from "../components/Three";
+import "./Accueil.css";
 
 export function Accueil() {
   return (
-    <div>
+    <div className="accueil">
       <LightRays
         raysOrigin="top-center"
         raysColor="#AD1B3B"
@@ -16,9 +17,53 @@ export function Accueil() {
         distortion={0.05}
         className="custom-rays"
       />
-      <ModelViewer />
+
       <h1>Bienvenue sur DanData</h1>
-      <p>Votre plateforme de données et d'analyses</p>
+      <div className="accueil-content">
+        <p>
+          Téléversez, visualisez, et analysez vos données avec un outil
+          polyvalent et puissant ou postez des articles pour la communauté!
+          <br />
+          <br />
+          Explorez une plateforme intuitive conçue pour simplifier la gestion de
+          vos données et favoriser le partage de connaissances.
+        </p>
+
+        <div className="model-wrapper">
+          <ModelViewer />
+        </div>
+
+        <p>
+          Partagez vos analyses et découvertes avec une communauté passionnée
+          par la data et l'innovation. Collaborez, apprenez et développez vos
+          compétences grâce à des outils puissants et une interface moderne.
+        </p>
+      </div>
+
+      <div className="BoiteDeBoite">
+        <div className="BoiteDeTexte">
+          <h2>XXX Utilisateurs nous font confiances</h2>
+          <p>
+            Aujourd’hui plus de XXX utilisateurs ont lancés leur site web en
+            faisant confiance à notre CMS.
+          </p>
+        </div>
+
+        <div>
+          <div className="BoiteDeTexte">
+            <h3>Visualisez</h3>
+            <p>Extrayez des statistiques d’après vos données</p>
+          </div>
+          <div className="BoiteDeTexte">
+            <h3>Importez vos données</h3>
+            <p>Extrayez des statistiques d’après vos données</p>
+          </div>
+          <div className="BoiteDeTexte">
+            <h2>Analysez</h2>
+            <p>Extrayez des statistiques d’après vos données</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
